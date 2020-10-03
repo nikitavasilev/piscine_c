548a5b5bac7894bc80b8e24ecb58ea5f39f476f5
Nikita VASILEV <vassilev.nikita@gmail.com>
2020-10-03 18:25:38 +0200

    clean ft + rm o

--- c09/ex01/includes/ft.h
+++ c09/ex01/includes/ft.h
@@ -1,22 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nvasilev <nvasilev@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/10/01 12:11:40 by nvasilev          #+#    #+#             */
-/*   Updated: 2020/10/01 12:13:20 by nvasilev         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef FT_H
 # define FT_H
 
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-int		ft_strcmp(char *s1, char *s2);
 int		ft_strlen(char *str);
 void	ft_swap(int *a, int *b);
+int		ft_strcmp(char *s1, char *s2);
+void	ft_putstr(char *str);
+void	ft_putchar(char c);
 
 #endif
